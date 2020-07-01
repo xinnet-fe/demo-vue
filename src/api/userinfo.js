0d0c47f3a43d4b6f042cf3f2d712bed704984da2
dongning2018 <dongning666666@163.com>
2020-07-01 14:00:35 +0800

    新知联调

--- src/api/userinfo.js
+++ src/api/userinfo.js
@@ -1,21 +1,33 @@
 import request from '@/utils/request'
+import axios from 'axios'
+
+const basePath = process.env.NODE_ENV === 'development' ? '' : '/admin'
 
 export function getUser(query) {
-  return request({
-    url: '/employee/getEmployeeInfo',
-    method: 'get',
-    params: query
-  })
+  return axios.get(`${basePath}/static/user.json`).then(res => res.data)
 }
 
 export function getSidebarMenus(query) {
-  return request({
-    url: '/navigation/listNavigation',
-    // url: 'static/data/menu.json',
-    method: 'get',
-    params: query
-  })
+  return axios.get(`${basePath}/static/menus.json`).then(res => res.data)
 }
+
+// export function getUser(query) {
+//   return request({
+//     url: '/employee/getEmployeeInfo',
+//     method: 'get',
+//     params: query
+//   })
+// }
+
+// export function getSidebarMenus(query) {
+//   return request({
+//     url: '/navigation/listNavigation',
+//     // url: 'static/data/menu.json',
+//     method: 'get',
+//     params: query
+//   })
+// }
+
 // 修改密码
 export function changePwd(data) {
   return request({
--- src/settings.js
+++ src/settings.js
@@ -53,7 +53,7 @@ module.exports = {
   hasDevelopment: process.env.NODE_ENV === 'development',
 
   // 研发环境token，要每天登录测试环境从浏览器cookie中获取
-  xbtoken: 'G7is2dSKYqIgQRwC5kI',
+  xbtoken: 'J6SHZjAknBEI6u3Phov',
 
   xbTokenKey: process.env.NODE_ENV === 'development' ? 'xbtoken' : 'xbtoken_id',
 
