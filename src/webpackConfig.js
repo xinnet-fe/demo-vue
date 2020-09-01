7ba941b3cedd0be5baa5a97ae4f2c9f35d6b279f
dongning2018 <dongning666666@163.com>
2020-09-01 10:07:34 +0800

    环境配置

--- src/webpackConfig.js
+++ src/webpackConfig.js
@@ -17,22 +17,27 @@ module.exports = {
   // },
   // devServer proxy
   proxy: {
-    '/employee': {
-      target: 'http://172.22.132.7:8083',
-      changeOrigin: true
-    },
-    '/advgroup': {
-      target: 'http://172.22.132.7:8083',
-      changeOrigin: true
-    },
-    '/normaladv': {
-      target: 'http://172.22.132.7:8083',
-      changeOrigin: true
-    },
-    '/admin': {
-      target: 'http://172.22.132.7:8024',
+    '/api': {
+      target: 'https://adv.xinnet.com',
+      secure: false,
       changeOrigin: true
     }
+    // '/employee': {
+    //   target: 'http://172.22.132.7:8083',
+    //   changeOrigin: true
+    // },
+    // '/advgroup': {
+    //   target: 'http://172.22.132.7:8083',
+    //   changeOrigin: true
+    // },
+    // '/normaladv': {
+    //   target: 'http://172.22.132.7:8083',
+    //   changeOrigin: true
+    // },
+    // '/admin': {
+    //   target: 'http://172.22.132.7:8024',
+    //   changeOrigin: true
+    // }
   },
 
   /**
