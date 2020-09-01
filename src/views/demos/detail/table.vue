c061d7d277a39f6356e601ee2dc9b188d76c5eae
dengyongliang <dyliang1985@hotmail.com>
2020-09-01 09:54:50 +0800

    20200901

--- src/views/demos/detail/table.vue
+++ src/views/demos/detail/table.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="demo-container" />
+  <div class="demo-container">
+    <el-descriptions :column="2" :labels="basicLabel" :data="basicInfo" />
+  </div>
 </template>
 
 <script>
@@ -8,108 +10,46 @@ export default {
   },
   data() {
     return {
-      tableData: [{
-        date: '2016-05-03',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333
-      }],
-      form: {
-        value: 0,
-        value1: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
-        value2: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
-        value3: '',
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
-        num: 1,
-        time: '',
-        startTime: '',
-        endTime: '',
-        options: [{
-          value: 'zhinan',
-          label: '指南',
-          children: [{
-            value: 'shejiyuanze',
-            label: '设计原则',
-            children: [{
-              value: 'yizhi',
-              label: '一致'
-            }, {
-              value: 'fankui',
-              label: '反馈'
-            }, {
-              value: 'xiaolv',
-              label: '效率'
-            }, {
-              value: 'kekong',
-              label: '可控'
-            }]
-          }, {
-            value: 'daohang',
-            label: '导航',
-            children: [{
-              value: 'cexiangdaohang',
-              label: '侧向导航'
-            }, {
-              value: 'dingbudaohang',
-              label: '顶部导航'
-            }]
-          }]
-        }]
+      basicLabel: {
+        agentCode: '商品名称',
+        company: '订单号',
+        market: '商品编号',
+        level: '是否退费',
+        phone: '产品类型',
+        email: '方式',
+        province: '服务编号',
+        city: '交易类型',
+        address: '商品类型',
+        address1: '代理编号',
+        address2: '规格',
+        address3: '操作时间',
+        address4: '金额',
+        address5: '销售人员',
+        address6: '成本价',
+        address7: '所属分公司',
+        address8: '域名',
+        address9: '财务归属'
       },
-      dialogImageUrl: '',
-      dialogVisible: false,
-      disabled: false
+      basicInfo: {
+        agentCode: 'safsd',
+        company: 'safd',
+        phone: '15110066180',
+        email: '',
+        province: '北京',
+        city: '北京',
+        level: '2级',
+        market: '商品编号',
+        address: '商品类型',
+        address1: '代理编号',
+        address2: '规格',
+        address3: '操作时间',
+        address4: '金额',
+        address5: '销售人员',
+        address6: '成本价',
+        address7: '所属分公司',
+        address8: '域名',
+        address9: '财务归属'
+      }
     }
   },
   methods: {
