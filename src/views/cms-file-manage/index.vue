67398b5a51718e7d78a2c625202e83b06d20b3d6
dongning2018 <dongning666666@163.com>
2020-09-09 15:58:10 +0800

    添加文件管理器模块

---
+++ src/views/cms-file-manage/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <iframe frameborder="0" class="elfinder" src="/elfinder.html" />
+</template>
+
+<style scoped lang="scss">
+.elfinder {
+  width: calc(100vw - 250px);
+  height: calc(100vh - 130px);
+  min-height: calc(100vh - 130px);
+}
+</style>
