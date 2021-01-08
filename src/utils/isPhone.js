cb0e00e7f3dd14ada3a5dcf2e809294dbe084681
wangyang <550759049@qq.com>
2021-01-08 11:07:27 +0800

    '初始代理营销'

--- src/utils/isPhone.js
+++
@@ -1,9 +0,0 @@
-import addRuleFunc from './addRuleFunc'
-
-export function isPhone(val) {
-  return /^1\d{10}$/.test(val)
-}
-
-isPhone.message = '手机号码格式有误'
-
-export default addRuleFunc(isPhone)
--- src/views/boss-navigation-yingxiaoservice/boss-navigation-ziliaoservice/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>ziliaoservice</div>
-</template>
